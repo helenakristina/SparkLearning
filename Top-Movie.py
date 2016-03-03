1910c97667a25258cc5db5b07494a6ad1ec888d6
Helena Lucia <helena.lucia@intel.com>
2016-03-03 10:13:58 -0800

    Added broadcast variable to use movie names from separate file to make top movies more readablae

--- Top-Movie.py
+++ Top-Movie.py
@@ -1,16 +1,31 @@
 from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+conf = SparkConf().setMaster("local").setAppName("TopMovie")
 sc = SparkContext(conf = conf)
 
 def parseLines(text):
     fields = text.split()
-    movieId = fields[1]
+    movieId = int(fields[1])
     return (movieId, 1)
 
+def loadMovieNames():
+    movieNames = {}
+    with open("ml-100k/u.ITEM") as f:
+        for line in f:
+            fields = line.split('|')
+            movieId = int(fields[0])
+            name = fields[1]
+            movieNames[movieId] = name
+    return movieNames
+
+movieNamesDict = sc.broadcast(loadMovieNames())
 
 lines = sc.textFile("ml-100k/u.data")
 total = lines.map(parseLines).reduceByKey(lambda x,y: x + y).map(lambda(x,y):(y,x)).sortByKey()
+totalsWithNames = total.map(lambda (count, movieId) : (movieNamesDict.value[movieId], count))
 
-results = total.collect()
-print "The top movie is MovieID %s and it was watched %i times" % (results[-1][1], results[-1][0])
+results = totalsWithNames.collect()
+
+for result in results:
+    print "The  movie %s was watched %i times" % (result[0], result[1])
+print "The top movie is %s and it was watched %i times" % (results[-1][0], results[-1][1])
